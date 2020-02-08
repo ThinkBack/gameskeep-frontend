e3f9a2be136b5fb92c102e62302d65d657b3ba6e
Alberto Jacinto <aljaga96@gmail.com>
2020-02-08 20:20:19 +0100

    Create basic scaffold for search-bar
    * Create basic scaffold for search-bar
    
    * Chore: Add pull request template
    
    * Add EOF
    
    Co-Authored-By: Iván Gálvez Rochel <ivan.galvez.rochel@gmail.com>
    
    * Add EOF 2
    
    Co-Authored-By: Iván Gálvez Rochel <ivan.galvez.rochel@gmail.com>
    
    Co-authored-by: Iván Gálvez Rochel <ivan.galvez.rochel@gmail.com>

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,15 @@
+### What
+
+_Provide a description about the goal of these changes_
+
+### How
+
+_Describe the big-picture of the implementation_
+
+### Screenshots
+
+_If this PR is UI related provide related screenshots to your changes_
+
+### Test
+
+_Provide instructions on how to test this changes_
---
+++ src/components/search/search.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './search.scss';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+    }
+  }
+
+  render () {
+    const { input } = this.state;
+
+    return (
+      <form className="search-bar">
+        <span className="search-bar search-bar__search-icon"> L </span>
+        <input type="text" value={input} className="search-bar search-bar__search-input"
+          onChange={e => this.setState({ input: e.target.value})}/>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
