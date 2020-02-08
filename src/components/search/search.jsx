abd132250f77a5cc2ac191317834ad0b3e649600
Alberto Jacinto <aljaga96@gmail.com>
2020-02-08 21:30:49 +0100

    Fix: Folder structure

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import './search.scss';
+import React from "react";
+import "./search.scss";
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      input: '',
-    }
+      input: ""
+    };
   }
 
-  render () {
+  render() {
     const { input } = this.state;
 
     return (
       <form className="search-bar">
         <span className="search-bar search-bar__search-icon"> L </span>
-        <input type="text" value={input} className="search-bar search-bar__search-input"
-          onChange={e => this.setState({ input: e.target.value})}/>
+        <input
+          type="text"
+          value={input}
+          className="search-bar search-bar__search-input"
+          onChange={e => this.setState({ input: e.target.value })}
+        />
       </form>
     );
   }
