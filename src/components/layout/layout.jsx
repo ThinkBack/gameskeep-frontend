023d2a639ff5071082f307335cf73447124e7fee
Iván Gálvez Rochel <ivan.galvez.rochel@gmail.com>
2020-02-09 10:50:03 +0100

    Added react-router

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import SearchBar from '../search/search.jsx';
+import NavBar from '../navbar/navbar.jsx';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './layout.scss';
 
@@ -12,8 +15,25 @@ class Layout extends React.Component {
         <div className="grid-container__header">
           <SearchBar />
         </div>
-        <div className="grid-container__navbar"></div>
-        <div className="grid-container__body"></div>
+        <div className="grid-container__navbar">
+          <NavBar />
+        </div>
+        <div className="grid-container__body">
+          <Switch>
+            <Route path="/pending">
+              <h1>PENDING</h1>
+            </Route>
+            <Route path="/playing">
+              <h1>PLAYING</h1>
+            </Route>
+            <Route path="/finished">
+              <h1>FINISHED</h1>
+            </Route>
+            <Route path="/">
+              <Redirect to="/pending" />
+            </Route>
+          </Switch>
+        </div>
       </div>
     );
   }
